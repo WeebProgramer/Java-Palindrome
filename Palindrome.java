5bf33e60837c40e426f01cf2b7158e32871a3861
WeebProgramer <119539638+WeebProgramer@users.noreply.github.com>
2025-10-03 11:41:25 +0200

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -1,10 +1,9 @@
 
 
-public class Palindrome {
+public class Palindrome (string str){
 
     public static void main(String[] args) {
         String Palindrome  = ""; // Temp variable to check the word in reverse 
-        String str = "ollo"; // Change this to check if the word is a palindrome 
 
 
         for (int i = 1; i <= str.length(); i++) {
